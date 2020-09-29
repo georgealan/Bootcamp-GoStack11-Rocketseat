9e4a0e0cebf1f3ea8329a59cf7cec473c28232a2
George Alan Rufo <georgealanrufo@gmail.com>
2020-09-29 00:44:40 -0300

    Inicio da versão web do sistema de barbearia GoBarber

---
+++ aulas/gobarber-web/src/pages/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content />
+    <Background />
+  </Container>
+);
+
+export default SignIn;
