6b558aa71d2b9daff4b5b0a7ed1fc72df718df5f
George Alan Rufo <georgealanrufo@gmail.com>
2020-09-29 23:02:39 -0300

    Continuando desenvolvimento do sistema de barbearia GoBarber

--- aulas/gobarber-web/src/pages/SignIn/index.tsx
+++ aulas/gobarber-web/src/pages/SignIn/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => (
   <Container>
-    <Content />
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Faça seu logon</h1>
+
+        <Input name="email" placeholder="E-mail" />
+
+        <Input name="password" type="password" placeholder="senha" />
+
+        <Button type="submit">Entrar</Button>
+
+        <a href="forget">Esqueci minha senha</a>
+      </form>
+
+      <a href="forget">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+
     <Background />
   </Container>
 );
